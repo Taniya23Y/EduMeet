e7bd5487cbb037e2dcd4d775bafb7941365f5002
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2024-11-10 19:31:13 +0530

    created: utils & auth middlewares

---
+++ 01_backend-edumeet/middlewares/auth.js
@@ -0,0 +1,13 @@
+const jwt = require("jsonwebtoken");
+
+// auth
+exports.auth = async (req, res, next) => {};
+
+// student
+exports.isStudent = async (req, res, next) => {};
+
+// instructor
+exports.isInstructor = async (req, res, next) => {};
+
+// admin
+exports.isAdmin = async (req, res, next) => {};
